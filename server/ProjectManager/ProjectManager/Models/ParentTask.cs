2217744970c0e4621be1fa38a9dbbce5c844d0b0
GaneshPDGIT <65563728+GaneshPDGIT@users.noreply.github.com>
2020-06-17 00:13:03 -0500

    Added Web APi- Server code
    Added Web APi- Server code

---
+++ server/ProjectManager/ProjectManager/Models/ParentTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectManager.Models
+{
+    public class ParentTask
+    {
+        public int ParentTaskId { get; set; }
+
+        public string ParentTaskName { get; set; }
+
+
+    }
+}
